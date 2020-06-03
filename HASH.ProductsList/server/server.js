fe4094fb0e6e67350c76d54ddb61d36de9a707d9
Gabriel Straube <ga.straube@gmail.com>
2020-06-03 18:51:34 -0300

    using environment variables to ports

--- HASH.ProductsList/server/server.js
+++ HASH.ProductsList/server/server.js
@@ -1,7 +1,9 @@
+var PORT = process.env.NODE_PORT || 3000 
+
 var http = require('http')
     ,app = require('./config/express');
 
-http.createServer(app).listen(3000, function() {
+http.createServer(app).listen(PORT, function() {
     console.log('Server listening the port: ' + this.address().port);
 });
 
